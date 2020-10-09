6ef4ad2be471fe6d750f3d6b2aa5da9c79fd76aa
suryanarayanan035 <suryanarayanan035@gmail.com>
2020-10-09 22:02:44 +0530

    postnet created

--- model/modules.py
+++ model/modules.py
@@ -20,4 +20,11 @@ def decoder_prenet(inputs,isTraining=False,name="decoder_prenet"):
         
     return output
 
-def 
+def postnet(inputs,is_training=False,name="postnet"):
+    conv_output = tf.expand_dims(inputs,-1)
+    for i in range(4):
+        conv_output = tf.keras.layers.Conv2D(filers=512,kernel_size=(1,5),
+                      padding="same",name=name+"_layer"+str(_),activation="tanh")(conv_output)
+        conv_output = tf.keras.layers.Droput(0.5,name=name + '_layer' + str(_) + "_dropout",training=is_training)(conv_output)
+
+    return conv_output
